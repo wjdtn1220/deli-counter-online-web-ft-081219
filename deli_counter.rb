7de737ba22e5d2e41b33963a40575501d15b84dd
Jeong Park <wjdtn1220@gmail.com>
2019-08-16 21:08:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,26 +1,26 @@
-def line(katzdeli)
+def line(katz_deli)
   if katzdeli.length == 0
     puts "The line is currently empty."
   else
     new_array = []
-    katzdeli.each do |person|
-      new_array << "#{(katzdeli.index(person).to_i + 1)}. #{person}"
+    katz_deli.each do |person|
+      new_array << "#{(katz_deli.index(person).to_i + 1)}. #{person}"
     end
     puts "The line is currently: #{new_array.join(" ")}"
   end
 end
 
-def take_a_number(katzdeli, name)
+def take_a_number(katz_deli, name)
   if 
   katzdeli << name
-  puts "Welcome #{name}. You are number #{katzdeli} in line."
+  puts "Welcome #{name}. You are number #{katz_deli} in line."
   end
 end
 
-def now_serving(katzdeli)
-  if katzdeli.length == 0
+def now_serving(katz_deli)
+  if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{katzdeli.shift}."
+    puts "Currently serving #{katz_deli.shift}."
   end
 end
