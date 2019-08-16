5f1c1964fae1d75c46a0a2a29233205bffa73ff9
Jeong Park <wjdtn1220@gmail.com>
2019-08-16 21:23:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,11 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    puts "The line is currently: 1. #{katz_deli[0]} 2. #{katz_deli[1]} 3. #{katz_deli[2]}"
+    new_array = []
+    katz_deli.each do |person|
+      new_array << "#{(katz_deli.index(person).to_i + 1)}. #{person}"
+    end
+    puts "The line is currently: #{new_array.join(" ")}"
   end
 end
 
