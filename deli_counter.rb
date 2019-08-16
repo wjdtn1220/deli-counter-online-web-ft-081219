1c16c8182b90cc2390c5a33e36a28d71974f7be9
Jeong Park <wjdtn1220@gmail.com>
2019-08-16 20:58:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,12 @@
 # Write your code here.
 
-def line(array)
-  if array.length == 0
+def line(katzdeli)
+  if katzdeli.length == 0
     puts "The line is currently empty."
   else
     new_array = []
-    array.each do |person|
-      new_array << "#{(array.index(person).to_i + 1)}. #{person}"
+    katzdeli.each do |person|
+      new_array << "#{(katzdeli.index(person).to_i + 1)}. #{person}"
     end
     puts "The line is currently: #{new_array.join(" ")}"
   end
