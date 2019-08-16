99ddc6fbac503d96ac1e713525538bcb51ba80d7
Jeong Park <wjdtn1220@gmail.com>
2019-08-16 21:03:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,7 @@ def take_a_number(katzdeli, name)
 end
 
 def now_serving(katzdeli)
-  if katzdeli == 0
+  if katzdeli.empty?
     puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{katzdeli.first}."
