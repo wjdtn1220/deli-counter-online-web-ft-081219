adcf64d3650418c9197318c4aa9def271e15fe77
Jeong Park <wjdtn1220@gmail.com>
2019-08-16 21:21:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     puts "The line is currently: 1. #{katz_deli[0]} 2. #{katz_deli[1]} 3. #{katz_deli[2]}"
+  end
 end
 
 def take_a_number(katz_deli, name)
