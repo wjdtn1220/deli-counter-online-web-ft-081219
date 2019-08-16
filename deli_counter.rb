71ee1afd185ece03af5f1c2a64b3a7447bea05d3
Jeong Park <wjdtn1220@gmail.com>
2019-08-16 21:13:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,7 +11,7 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-  katz_deli << name
+  katz_deli.push(name)
   new_spot = katz_deli.index(name) + 1
   puts "Welcome #{name}. You are number #{new_spot} in line."
 end
