4b34cb4b2ceefff8950ecc42954f2746918e7220
Jeong Park <wjdtn1220@gmail.com>
2019-08-16 21:21:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    puts "The line is currently: 1. #{katz_deli[0]} 2. #{katz_deli[1] 3. #{katz_deli[2]"
+    puts "The line is currently: 1. #{katz_deli[0]} 2. #{katz_deli[1]} 3. #{katz_deli[2]}"
 end
 
 def take_a_number(katz_deli, name)
