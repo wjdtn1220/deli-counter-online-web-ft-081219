d55d7005bab0897924d707049895e11854e15041
Jeong Park <wjdtn1220@gmail.com>
2019-08-16 20:52:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+  else
+    new_array = []
+    array.each do |person|
+      new_array << "#{(array.index(person).to_i + 1)}. #{person}"
+    end
+    puts "The line is currently: #{new_array.join(" ")}"
+  en
+end
